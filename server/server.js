ca867f762ddd49ffbdbb8df41988b133f91008ab
Amit <amitthakurat4615321@gmail.com>
2025-05-25 14:55:06 +0530

    minor fix

--- server/server.js
+++ server/server.js
@@ -16,7 +16,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+const corsOptions = {
+  origin: "*",
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Routes
